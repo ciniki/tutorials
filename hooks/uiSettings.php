30cebb677aee694eb0f23e95273a8b836c01ef3c
Andrew Rivett <veggiefrog@gmail.com>
2016-09-10 11:27:31 -0400

    Added hooks/uiSettings for new business menu

---
+++ hooks/uiSettings.php
@@ -0,0 +1,42 @@
+<?php
+//
+// Description
+// -----------
+// This function will return a list of user interface settings for the module.
+//
+// Arguments
+// ---------
+// ciniki:
+// business_id:     The ID of the business to get tutorials for.
+//
+// Returns
+// -------
+//
+function ciniki_tutorials_hooks_uiSettings($ciniki, $business_id, $args) {
+
+    //
+    // Setup the default response
+    //
+    $rsp = array('stat'=>'ok', 'menu_items'=>array());
+
+    //
+    // Check permissions for what menu items should be available
+    //
+    if( isset($ciniki['business']['modules']['ciniki.tutorials'])
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['employees'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $menu_item = array(
+            'priority'=>1300,
+            'label'=>'Tutorials', 
+            'edit'=>array('app'=>'ciniki.tutorials.main'),
+            );
+        $rsp['menu_items'][] = $menu_item;
+    } 
+
+    return $rsp;
+}
+?>
